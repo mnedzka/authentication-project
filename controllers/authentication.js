1cd16cb70435f51db3997d86b2ed0225be7b7316
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-25 22:20:58 +0100

    Add bcrypt and encrypt user password with it

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,19 @@
 const User = require("../models/user");
+const jwt = require("../models/user");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, cofig.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const passowrd = req.body.passowrd;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Please provide email and password" });
+  }
   // See if a user with the given email exist
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) return next(err);
@@ -20,7 +31,7 @@ exports.signup = function(req, res, next) {
       if (err) return next(err);
 
       // Respond to request indicating that user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
