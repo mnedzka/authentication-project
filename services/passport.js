be6b5f35c7f8be053522a6ce36f5b171a4fc9642
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-26 23:16:58 +0100

    Create local strategy with passport for comparing user's passwords when he/she sigin

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,27 @@ const User = require("../models/user");
 const config = require("..config");
 const JwtStartegy = require("passport-jwt").Startegy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-locsl");
+
+// Create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if (!user) return done(null, false);
+
+      return done(null, user);
+    });
+  });
+});
 
 // Set up option for JWT startegy
 const jwtOptions = {
@@ -25,3 +46,4 @@ const jwtLogin = new JwtStartegy(jwtOptions, function(payload, done) {
 
 //Use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
